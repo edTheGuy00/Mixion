8065636788af6c7d74422d40d5f58628903349be
edgar <edgrr.trem000@gmail.com>
2018-02-02 21:56:24 -0600

    Work on Loading comments

--- app/src/main/java/com/taskail/mixion/steemdiscussion/DiscussionRecyclerViewAdapter.kt
+++ app/src/main/java/com/taskail/mixion/steemdiscussion/DiscussionRecyclerViewAdapter.kt
@@ -13,22 +13,43 @@ import com.taskail.mixion.R
 class DiscussionRecyclerViewAdapter(@NonNull private val discussionLayout: View) :
         RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private var isLoading = true
+    private var noComments = false
+
     class DiscussionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-    override fun getItemViewType(position: Int): Int {
-        if (position == 0){
-            return R.layout.layout_discussion_details
+        fun setComment(){
+
         }
 
-        return R.layout.card_comments
+    }
+
+    override fun getItemViewType(position: Int): Int {
+
+        return when (position){
+            0 -> return R.layout.layout_discussion_details
+            1 -> {
+                if (isLoading)
+                    return R.layout.item_loading
+                else R.layout.card_comments
+                if (noComments)
+                    return R.layout.item_no_comments
+                else R.layout.card_comments
+            }
+            else -> {
+                R.layout.card_comments
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
 
         return when (viewType){
             R.layout.layout_discussion_details -> DiscussionViewHolder(discussionLayout)
+            R.layout.card_comments -> CommentViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_comments,
+                    parent, false))
             else -> {
                 CommentViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_comments,
                         parent, false))
@@ -37,7 +58,7 @@ class DiscussionRecyclerViewAdapter(@NonNull private val discussionLayout: View)
     }
 
     override fun getItemCount(): Int {
-        var count = 1 // the discussion details
+        var count = 1 // discussion details
 
         return count
     }
