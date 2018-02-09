50c0c6ed9ebfbcebeac5a6225e38d97e069807ce
edgar <edgrr.trem000@gmail.com>
2018-02-09 16:07:40 -0600

    Import nano Markdown highlighter colors

---
+++ app/src/main/java/com/taskail/mixion/markortexteditor/MarkdownHighlighterColors.kt
@@ -0,0 +1,32 @@
+package com.taskail.mixion.markortexteditor
+
+/**
+ *Created by ed on 2/8/18.
+ */
+class MarkdownHighlighterColors {
+    private val COLOR_HEADER = -0x109300
+    private val COLOR_LINK = -0xe15c02
+    private val COLOR_LIST = -0x255adf
+    private val COLOR_QUOTE = -0x774fb4
+    private val COLOR_DOUBLESPACE = -0x1f1e20
+
+    fun getHeaderColor(): Int {
+        return COLOR_HEADER
+    }
+
+    fun getLinkColor(): Int {
+        return COLOR_LINK
+    }
+
+    fun getListColor(): Int {
+        return COLOR_LIST
+    }
+
+    fun getDoublespaceColor(): Int {
+        return COLOR_DOUBLESPACE
+    }
+
+    fun getQuotationColor(): Int {
+        return COLOR_QUOTE
+    }
+}
